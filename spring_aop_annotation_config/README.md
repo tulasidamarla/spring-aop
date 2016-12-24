6c6e3de55200279427865b0a92de137c1abf53b2
tulasi <ram.damarla@gmail.com>
2016-12-25 00:35:05 +0530

    aspect code changes

---
+++ spring_aop_annotation_config/README.md
@@ -0,0 +1,3 @@
+For AOP configuration in java, app config java class needs to be annotated with @EnableAspectJAutoProxy.
+
+Note: All other configuration of log4j, aspecjrt, aspecjweaver dependencies have to be added in pom.xml file. 
--- spring_aop_annotation_config/src/main/java/com/springsample/config/AppConfig.java
+++ spring_aop_annotation_config/src/main/java/com/springsample/config/AppConfig.java
@@ -1,30 +1,13 @@
 package com.springsample.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import com.springsample.repository.CustomerRepository;
-import com.springsample.repository.CustomerRepositoryImpl;
-import com.springsample.service.CustomerService;
-import com.springsample.service.CustomerServiceImpl;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+@EnableAspectJAutoProxy
+@ComponentScan(basePackages={"com.springsample"})
 public class AppConfig {
 	
-		@Bean(name = "customerService")
-		public CustomerService getCustomerService() {
-			CustomerServiceImpl service = new CustomerServiceImpl();
-			// Incase of setter injection
-			 service.setCustomerRepository(getCustomerRepository());
-			// Incase of constructor injection
-			// Customerservice service = new
-			// CustomerServiceImpl(getCustomerRepository());
-			return service;
-		}
-
-		@Bean(name = "customerRepository")
-		public CustomerRepository getCustomerRepository() {
-			return new CustomerRepositoryImpl();
-		}
-	
+		
 }
