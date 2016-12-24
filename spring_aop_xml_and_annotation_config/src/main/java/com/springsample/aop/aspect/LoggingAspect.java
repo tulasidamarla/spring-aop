9be01d337e8ad50de8fa1dabe5ac26a95d1c814d
tulasi <ram.damarla@gmail.com>
2016-12-25 00:50:13 +0530

    updated import to org.aspectj.lang.JoinPoint

--- spring_aop_xml_and_annotation_config/src/main/java/com/springsample/aop/aspect/LoggingAspect.java
+++ spring_aop_xml_and_annotation_config/src/main/java/com/springsample/aop/aspect/LoggingAspect.java
@@ -1,6 +1,8 @@
 package com.springsample.aop.aspect;
 
+
 import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -11,7 +13,7 @@ public class LoggingAspect {
 	private static final Logger logger = Logger.getLogger(LoggingAspect.class);
 
 	@Before("execution(* getCustomers())")
-	public void startLog(){
-		logger.info("entering the method");
+	public void startLog(JoinPoint jp){
+		logger.info("entering the method " + jp.getStaticPart().getClass().getName());
 	}
 }
