9c43757c06fcdccf88b554592992b0c147a72762
tulasi <ram.damarla@gmail.com>
2016-12-25 12:08:53 +0530

    Added @After code

--- spring_aop_annotation_config/src/main/java/com/springsample/aop/aspect/LoggingAspect.java
+++ spring_aop_annotation_config/src/main/java/com/springsample/aop/aspect/LoggingAspect.java
@@ -3,6 +3,7 @@ package com.springsample.aop.aspect;
 
 import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -13,7 +14,12 @@ public class LoggingAspect {
 	private static final Logger logger = Logger.getLogger(LoggingAspect.class);
 
 	@Before("execution(* getCustomers())")
-	public void startLog(JoinPoint jp){
-		logger.info("entering the method " + jp.getStaticPart().getClass().getName());
+	public void enterLog(JoinPoint jp){
+		logger.info("entering the method " + jp.getStaticPart().getSignature().toString());
+	}
+	
+	@After("execution(* getCustomers())")
+	public void exitLog(JoinPoint jp){
+		logger.info("exiting the method " + jp.getStaticPart().getSignature().toString());
 	}
 }
--- spring_aop_annotation_config/src/main/java/com/springsample/main/Application.java
+++ spring_aop_annotation_config/src/main/java/com/springsample/main/Application.java
@@ -9,13 +9,9 @@ import com.springsample.service.CustomerService;
 public class Application {
 
 	public static void main(String[] args) {
-
 		ApplicationContext ctx= new AnnotationConfigApplicationContext(AppConfig.class);
-		
 		CustomerService service = ctx.getBean("customerService", CustomerService.class);
 		System.out.println(service.getCustomers().get(0).getFirstName());
 		System.out.println(service.getCustomers().get(0).getLastName());
-		
 	}
-
 }
